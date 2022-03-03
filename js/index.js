f9836147c7c21c658890a77b6b763e2dbe080fac
Neerajkumar <ny587227@gmail.com>
2022-03-03 10:19:47 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+var hour=document.getElementById("hour")
+var minute=document.getElementById("minute")
+var second=document.getElementById("second")
+setInterval(()=>{
+    let date=new Date()
+    let htime=date.getHours()
+    let mtime=date.getMinutes()
+    let stime=date.getSeconds()
+    let hrotate=30*htime+0.5*mtime
+    let mrotate=6*mtime+0.1*stime
+    let srotate=6*stime
+
+    hour.style.transform=`rotate(${hrotate}deg)`
+    minute.style.transform=`rotate(${mrotate}deg)`
+    second.style.transform=`rotate(${srotate}deg)`
+    
+},1000)
