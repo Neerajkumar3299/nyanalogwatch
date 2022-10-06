9f4e199e9c0f756aafca1c24477e4306cab0e801
Neeraj Yadav <ny587227@gmail.com>
2022-10-06 11:41:33 +0530

    inital commit

--- js/index.js
+++ js/index.js
@@ -7,11 +7,12 @@ setInterval(()=>{
     let mtime=date.getMinutes()
     let stime=date.getSeconds()
     let hrotate=30*htime+0.5*mtime
+    console.log(hrotate)
     let mrotate=6*mtime+0.1*stime
     let srotate=6*stime
 
     hour.style.transform=`rotate(${hrotate}deg)`
+    hour.style.transformOrigin="bottom"
     minute.style.transform=`rotate(${mrotate}deg)`
     second.style.transform=`rotate(${srotate}deg)`
-    
 },1000)
